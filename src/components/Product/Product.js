cc6a6469c14ffe7f0dc494b7c2f0b2b150439bac
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-06-06 01:13:44 +0600

    product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import './Product.css';
 const Product = (props) => {
   console.log(props.product.name)
+  const { img, name, seller, price, stock } = props.product;
   return (
-    <div>
-      <h3>{props.product.name}</h3>
+    <div className='product'>
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <h3 class='product-name'>{name}</h3>
+        <br />
+        <p><small>by:{seller}</small></p>
+        <p>${price}</p>
+        <br />
+        <p><small>only{stock}left in stock-order soon</small></p>
+      </div>
+
     </div>
   );
 };
