7fd0923766319bbedf2927acff6620d8464c09ba
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-06-18 09:01:55 +0600

    first commit

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,27 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import { getShoppingCart } from '../../utilities/fakedb';
+import { getShoppingCart ,removeFromDb} from '../../utilities/fakedb';
 import fakeData from '../../fakeData';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
   const [cart,setCart]=useState([]);
-       useEffect(()=>{
+
+  const removeProduct=(productkey)=>{
+    
+    const newCart=cart.filter(product=>product.key !== productkey);
+    setCart(newCart);
+    removeFromDb(productkey);
+
+  }
+       useEffect( () => {
     //cart
     const savedCart=getShoppingCart();
     const productKeys=Object.keys(savedCart);
     const cartProducts=productKeys.map(key=> {
       const product=fakeData.find(product=> product.key===key);
       product.quantity=savedCart[key];
+      
       return product;
-    }, []);
+    } )
     setCart(cartProducts);
-  })
+  },[])
   return (
     <div>
-      <h1>item Review:{cart.length}</h1>
+      
       
       {
-        cart.map(product=><ReviewItem product={product}></ReviewItem>)
+        cart.map(product=>
+        <ReviewItem
+        key={product.key}
+         product={product}
+          removeProduct={removeProduct}
+          >
+         </ReviewItem>)
       }
     </div>
   );
