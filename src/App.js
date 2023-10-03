49de9f5ac22840a9420361822f49c8fdeadd834b
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-10-04 00:32:23 +0600

    private Route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import NotFound from './components/NotFound/NotFound';
 import ProductDetail from './components/ProductDetail/ProductDetail';
 import Shipment from './components/Shipment/Shipment';
 import Login from './components/Login/Login';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 export const UserContext = createContext ();
 
@@ -36,9 +37,9 @@ const [loggedInUser,setLoggedInUser] = useState ({});
           <Route path='/inventory'>
             <Inventory></Inventory>
           </Route>
-          <Route path='/Shipment'>
+          <PrivateRoute path='/Shipment'>
             <Shipment></Shipment>
-          </Route>
+          </PrivateRoute>
           <Route path='/Login'>
             <Login></Login>
           </Route>
