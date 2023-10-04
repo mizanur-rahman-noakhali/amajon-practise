1e91711d0c9493d67171d1185d862836f904d54a
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-10-04 23:51:25 +0600

    hellow

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from '/web developement/projects/amajon-practise/src/images/logo.png'
 import '../Header.css'
 import { Link } from 'react-router-dom/cjs/react-router-dom';
+import { UserContext } from '../../App';
 const Header = () => {
+
+  const [loggedInUser,setLoggedInUser]= useContext(UserContext);
   return (
     <div class='header'>
       
@@ -14,6 +17,7 @@ const Header = () => {
           {/* <Link to='/shop'>shop</Link>
           <Link to='/review'>Review</Link>
           <Link to='/inventory'>Manage Inventory</Link> */}
+          <button onClick={()=> setLoggedInUser({})}>SignOut</button>
         </nav>
     </div>
   );
